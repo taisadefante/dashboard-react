cefcfc2dffd4ce39174f7d909ec187c8c68b5bb8
tais defante <taisadefante@hotmail.com>
2024-06-21 15:04:35 -0300

    primeiro grafico

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React, { useState } from "react";
 import "./App.css";
+import Chart from "react-google-charts";
 
 function App() {
-  return <h1>Dashboard</h1>;
+  const TITULO = "Quantidae de cadastro primeiro semestre";
+  const [dados, setDados] = useState([
+    ["Mês", "Quantidade"],
+    ["Janeiro", 33],
+    ["Fevereiro", 68],
+    ["Março", 49],
+    ["Abril", 15],
+    ["Maio", 80],
+    ["Junho", 27],
+  ]);
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+
+      <Chart
+        width={"400px"}
+        height={"300px"}
+        chartType={"PieChart"}
+        data={dados}
+        options={{
+          title: TITULO,
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
